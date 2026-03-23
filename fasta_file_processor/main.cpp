f84aa263c4dc8b841a1d4075c8a2777161259e6f
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2026-03-23 16:19:08 +0700

    add instances of how to write to and read from a file in main.cpp (will delete later)

--- fasta_file_processor/main.cpp
+++ fasta_file_processor/main.cpp
@@ -7,13 +7,21 @@ using namespace std;
 
 int main()
 {
+	// write to a file
 	string test_input;
 	cout << "Please input a random sequence: ";
 	cin >> test_input;
 	
-	ofstream file("result.txt");
-	file << test_input;
-	file.close();
+	ofstream out_file("result.txt");
+	out_file << test_input;
+	out_file.close();
+	
+	// read from a file
+	string test_output;
+	ifstream in_file("result.txt");
+	in_file >> test_output;
+	cout << test_output << endl;
+	in_file.close();
 	
 	return 0;
 }
