91e2f2bb3fb04c8328a135c9492cb1d2f4e9355d
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2026-03-26 20:28:07 +0700

    add function lines_counter()

--- fasta_file_processor/main.cpp
+++ fasta_file_processor/main.cpp
@@ -9,21 +9,31 @@ using namespace std;
 // Prototypes
 string read_file();
 int headlines_count(string text);
+int lines_counter(string content);
+
 
 int main()
 {
 	// TO-DO: Open the .fasta file and read it into a string
 	string content = read_file();
 	
-	// TO-DO: Iterate throught the whole text to count how many headlines are there
+	// TO-DO: Iterate through the whole text to count how many headlines are there
 	int headlines = headlines_count(content);
+	
+	// TO-DO: Figuring out how many lines are there
+	int lines = lines_counter(content);
+	
+	// TO-DO: Iterate through the whole text and save the content without the headlines
+	// to a string
 	
 	cout << content << '\n';
 	cout << "Number of headlines: " << headlines << '\n';
+	cout << "Number of lines: " << lines << '\n';
 	
 	return 0;
 }
 
+// Open the .fasta file and read it into a string
 string read_file()
 {
 	string result;
@@ -41,6 +51,7 @@ string read_file()
 	return result;
 }
 
+// Iterate through the whole text to count how many headlines are there
 int headlines_count(string text)
 {
 	int n = 0;
@@ -55,4 +66,21 @@ int headlines_count(string text)
 	}
 	
 	return n;
+}
+
+// Figuring out how many lines are there
+int lines_counter(string content)
+{
+	int n = 0;
+	
+	for (int i = 0; i < content.length(); i++)
+	{
+		if (content[i] == '\n')
+		{
+			n += 1;
+		}
+	}
+	
+	// Plus 1 because the last line doesn't have a '\n'
+	return (n + 1);
 }
