9a3c59818f853077bac500fee6ccc09367e819ff
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2026-03-27 15:30:56 +0700

    add function read_file_except_headlines

--- fasta_file_processor/main.cpp
+++ fasta_file_processor/main.cpp
@@ -8,13 +8,21 @@ using namespace std;
 
 // Prototypes
 string read_file();
+string read_file_except_headlines();
 
 
 int main()
 {
 	// TO-DO: Open the .fasta file and read it into a string
 	string content = read_file();	
-	cout << content << '\n';
+	
+	// TO-DO: Open the .fasta file and read it into a string EXCEPT for the headlines
+	string sequence = read_file_except_headlines();
+	
+	// Printing results
+	cout << "With headline\n" << content << '\n' << '\n';
+	cout << "Without headline\n" << sequence << '\n';
+	
 
 	return 0;
 }
@@ -37,3 +45,24 @@ string read_file()
 	return result;
 }
 
+// Open the .fasta file and read it into a string EXCEPT for the headlines
+string read_file_except_headlines()
+{
+	string temp; // To store the headlines
+	string line;
+	string result;
+	
+	ifstream input_file("sequences.fasta");
+	
+	if (input_file.is_open())
+	{
+		getline(input_file, temp);
+		while (getline(input_file, line))
+		{
+			result += (line + '\n');
+		}
+		input_file.close();
+	}
+	
+	return result;
+}
