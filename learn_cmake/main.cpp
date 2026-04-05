317fbe66c06d8208411346915c14a871945b1abe
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2026-04-05 14:41:43 +0700

    add main.cpp

---
+++ learn_cmake/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "src/math_utils.h"
+#include "src/string_utils.h"
+
+int main()
+{
+    std::cout << add(3, 4) << std::endl;
+    std::cout << repeat("hood", 4) << std::endl
+    return 0;
+}
